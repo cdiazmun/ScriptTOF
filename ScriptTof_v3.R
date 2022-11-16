93acc3494b59a2933387ba6f2ce992a254363100
Cristian Díaz-Muñoz <38433538+cdiazmun@users.noreply.github.com>
2022-11-16 11:39:01 +0100

    V3.2
    The presence of Toluene-D8 is now checked.

--- ScriptTof_v3.R
+++ ScriptTof_v3.R
@@ -17,13 +17,23 @@ library(tidyverse) # Type of R syntax, to make code easier
 library(reshape2)
 
 # Set the working directory
-setwd("C:/Users/cdiazmun/OneDrive - Vrije Universiteit Brussel/IMDO/R scripts IMDO/GC-TOF-MS/")
-samples_wd <- "C:/Users/cdiazmun/OneDrive - Vrije Universiteit Brussel/IMDO/R scripts IMDO/GC-TOF-MS/Sample reports/"
+setwd("C:/Users/mmorenoc/Vrije Universiteit Brussel/Cristian DIAZ MUNOZ - Mery/TOF")
+samples_wd <- "C:/Users/mmorenoc/Vrije Universiteit Brussel/Cristian DIAZ MUNOZ - Mery/TOF/SSR/"
 
 # List all files in the folder (all your samples)
 files <- list.files(path = samples_wd, pattern=".xls")
-names <- as.vector(sapply(strsplit(basename(files), ".xls"), `[`, 1))
-
+#names <- as.vector(sapply(strsplit(basename(files), ".xls"), `[`, 1))
+names_2 <- c(rep(c("a", "b","c"),27))
+names_3 <- c(rep("Captain_1", 3),rep("Captain_2", 3),rep("Captain_3", 3),
+             rep("Karma_1", 3),rep("Karma_2", 3),rep("Karma_3", 3),
+             rep("Thylbert_1", 3),rep("Thylbert_2", 3),rep("Thylbert_3", 3),
+             rep("Rish_1", 3),rep("Rish_2", 3),rep("Rish_3", 3),
+             rep("Gremline_1", 3), rep("Gremline_2", 3), rep("Gremline_3", 3),
+             rep("Yaya_1", 3),rep("Yaya_2", 3),rep("Yaya_3", 3),
+             rep("Smile_1", 3),rep("Smile_2", 3),rep("Smile_3", 3),
+             rep("Yuguen_1", 3),rep("Yuguen_2", 3),rep("Yuguen_3", 3),
+             rep("Bekombucha_1", 3),rep("Bekombucha_2", 3),rep("Bekombucha_3", 3))
+names_f <- paste(names_3,names_2, sep = "_")
 
 ##########################################################
 #1) Reshape the dataframe and get the best hit for each RT
@@ -31,7 +41,7 @@ names <- as.vector(sapply(strsplit(basename(files), ".xls"), `[`, 1))
 
 for (i in 1:length(files)) {
   # Import all files
-  df <- read_excel(path = paste("Sample reports/",files[i], sep = ""), skip=32) # 32 first rows are always empty, so can be skipped
+  df <- read_excel(path = paste("SSR/",files[i], sep = ""), skip=32) # 32 first rows are always empty, so can be skipped
   # Select the RT rows
   rt <- as.numeric(df[,12]=="DBC TIC") 
   # In which position they are
@@ -46,7 +56,7 @@ for (i in 1:length(files)) {
   # Modify the column names
   colnames(df1) <- c("Compound", "CAS", "RT", "MF", "Area")
   # Write the table in tsv format (easy to work with in R and Excel)
-  write.table(file = paste("Sample reports/",names[i],".txt", sep = ""), 
+  write.table(file = paste("SSR/",names_f[i],".txt", sep = ""), 
               x = df1, sep = "\t", dec = ".", quote = FALSE, row.names = FALSE)
 }
 
@@ -57,33 +67,37 @@ for (i in 1:length(files)) {
 
 # List all files in the folder (all your samples)
 files2 <- list.files(path = samples_wd, pattern=".txt")
+names_f2 <- sort(names_f)
 
 df_all <- data.frame(matrix(ncol = 7, nrow = 0))
 colnames(df_all) <- c("Compound", "CAS", "RT", "MF", "Area", "Sample","Duplicated")
 
 for (i in 1:length(files2)) {
   # Import all the files
-  df1 <- read.delim(file = paste("Sample reports/",files2[i], sep = ""))
+  df2 <- read.delim(file = paste("SSR/",files2[i], sep = ""))
   # Put the sample name
-  df1$Sample <- rep(names[i], nrow(df1))
+  df2$Sample <- rep(names_f2[i], nrow(df2))
   # Some compounds can be found multiple times: list all of them up
-  df1$Duplicated <- duplicated(df1$Compound)
+  df2$Duplicated <- duplicated(df2$Compound)
   # In which position they are
-  index2 <- which(duplicated(df1$Compound))
+  index2 <- which(duplicated(df2$Compound))
   # Add an '*' to every compound that is found twice (two *'s if it is found 3 times, etc.)
   while (length(index2)>0){ 
     for (k in index2){
-      df1$Compound[k] <- str_c(df1$Compound[k],"*")
+      df2$Compound[k] <- str_c(df2$Compound[k],"*")
     }
-    index2 <- which(duplicated(df1$Compound))
+    index2 <- which(duplicated(df2$Compound))
   }
-  df_all <- rbind(df_all,df1)
+  df_all <- rbind(df_all,df2)
 }
 
 # Write the table in tsv format (easy to work with in R and Excel)
 write.table(file = "Output reports/all_samples.txt", 
             x = df_all, sep = "\t", dec = ".", quote = FALSE, row.names = FALSE)
 
+# Check IS (Toluene-D8)
+check_list <- df_all$Sample[df_all$Compound == "Toluene-D8"]
+print(paste("There are ", length(files2) - length(check_list), " files with no Toluene-D8"))
 
 ################################################################
 #3) Eliminate those compounds present in only 1 out of 3 samples
@@ -91,8 +105,10 @@ write.table(file = "Output reports/all_samples.txt",
 
 # Add a column with the triplicate info (change according to the name of the file)
 df_tri <- df_all
-df_tri$Rep <- sapply(strsplit(basename(as.character(df_tri$Sample)), "_"), `[`, 2)
-df_tri$Sample <- sapply(strsplit(basename(as.character(df_tri$Sample)), "_"), `[`, 1)
+df_tri$Rep <- sapply(strsplit(basename(as.character(df_tri$Sample)), "_"), `[`, 3)
+df_tri$Sample <- paste(sapply(strsplit(basename(as.character(df_tri$Sample)), "_"), `[`, 1),
+                       sapply(strsplit(basename(as.character(df_tri$Sample)), "_"), `[`, 2),
+                       sep = " ")
 
 # Calculate the standard deviation for each compound in the triplicate. 
 df_tri <- df_tri %>%
@@ -124,3 +140,71 @@ df_av <- df_cur %>%
 # Write the table in tsv format (easy to work with in R and Excel)
 write.table(file = "Output reports/all.samples.curated.averaged.txt", 
             x = df_av, sep = "\t", dec = ".", quote = FALSE, row.names = FALSE)
+
+
+##############################################
+# 5) Plot the results as a PCA
+##############################################
+
+# Create the matrix of areas with compounds in the columns and samples in the rows
+tof <- df_av[c(1,2,5)] %>%
+  spread(key = Compound, value = Area)
+
+tof[is.na(tof)] <- 0
+
+# Write the table
+write.table(file = "Output reports/all.samples.curated.averaged.matrix.txt", 
+            x = tof, sep = "\t", dec = ".", quote = FALSE, row.names = FALSE)
+
+# Eliminate Bekombucha and divide all columns by Toluene-D8
+tof2 <- tof[-(1:3),] %>%
+  mutate_if(is.numeric, funs(./`Toluene-D8`))
+
+tof2$Bottle <- sapply(strsplit(basename(as.character(tof2$Sample)), " "), `[`, 1)
+
+# Remove the Toluene-D8 column
+
+tof2 <- tof2[-327]
+
+library(ggfortify) # Package needed for plotting the PCA
+
+# If the variances of the variables in the data vary by orders of magnitude, 
+# scale is appropriate
+ar.pca <- prcomp(tof2[2:(ncol(tof2)-1)],
+                 center = TRUE,
+                 scale. = FALSE)
+
+# Explore the factor loadings of the PCA
+print(ar.pca)
+# Explore the contribution of each PC to the total variance in the dataset
+screeplot(ar.pca)
+
+# Write the factor loadings to a csv file, if you want
+write.table(ar.pca,"pca_loading_factors.txt")
+
+# Plot the PCA (score plot)
+g <- autoplot(ar.pca, data = tof2, colour = "Bottle") +
+  theme_bw() + scale_colour_brewer(type = "qual", palette = "Set1")
+
+print(g)
+
+# Save the PCA in 300 DPI, fitting publication standards
+ggsave("PCA_fertemp_all_curated.tiff", width = 6, height = 4, units = "in", dpi = 300)
+
+# Plot the factor loadings (score plot + loading plot)
+h <- autoplot(ar.pca, data = na.omit(df), colour = "Brewery", shape = "Brewery", 
+              loadings = 5, loadings.colour = "black", loadings.label = TRUE, 
+              loadings.label.size = 3, loadings.label.colour = "Black",
+              loadings.label.repel = TRUE, lable = T) +
+  theme_bw()
+
+print(h)
+
+# Save the PCA in 300 DPI, fitting publication standards
+ggsave("PCA_metabolite_loadings_all.tiff", width = 6, height = 4, units = "in", dpi = 300)
+
+
+
+
+
+
